b9c9e801ac813cdb31902e2b5a3395c8a8c6a561
Кирилл Жумарин <azq2@ya.ru>
2014-07-19 15:31:04 +0000

    color selector fix

--- l2sme/www/templates/header.xhtml.php
+++ l2sme/www/templates/header.xhtml.php
@@ -5,10 +5,10 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="static/js/soundmanager/script/soundmanager2-nodebug-jsmin.js"></script>
 
-		<link rel="stylesheet" media="screen" type="text/css" href="static/css/colorpicker.css" />
 		<script type="text/javascript" src="static/js/colorpicker.js"></script>
 		<script type="text/javascript" src="static/js/functions.js?<?= $rev ?>"></script>
 		<link rel="stylesheet" media="screen" type="text/css" href="static/css/main.css?<?= $rev ?>" />
+		<link rel="stylesheet" media="screen" type="text/css" href="static/css/colorpicker.css" />
 	</head>
 	
 	<body>
