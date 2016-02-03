840dddcf99bd5ce7c59f1a100e95607ab97e093d
azq2 <azq2@ya.ru>
2016-02-03 19:30:19 +0200

    mobtop

--- l2sme/www/templates/footer.xhtml.php
+++ l2sme/www/templates/footer.xhtml.php
@@ -5,8 +5,7 @@
 			<noscript><a href="http://mobtop.ru/in/91077"><img src="http://mobtop.ru/91077.gif" alt="MobTop.Ru - рейтинг мобильных сайтов"/></a></noscript>
 		</center>
 <?php else: ?>
-	<center style="z-index:999999999999999;bottom:0;left:0;right:0;position:absolute;">
-		<a href="http://zhumarin.ru"><?= date("Y") ?> &copy; Kirill Zhumarin</a><br />
+	<center style="z-index:999999999999999;bottom:0;left:0;right:0;position:absolute">
 		<script type="text/javascript" src="http://mobtop.ru/c/91076.js"></script>
 		<noscript><a href="http://mobtop.ru/in/91076"><img src="http://mobtop.ru/91076.gif" alt="MobTop.Ru - рейтинг мобильных сайтов"/></a></noscript>
 	</center>
